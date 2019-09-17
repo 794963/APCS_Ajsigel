873f8113a1416f25b73a914f2a802dfdbce69890
794963 <51171863+794963@users.noreply.github.com>
2019-09-16 18:35:25 -0700

    finsihing up stuf

--- LabOneReview/ExamOneReview.java
+++ LabOneReview/ExamOneReview.java
@@ -8,30 +8,29 @@
 public class ExamOneReview
 {
     // instance variables - replace the example below with your own
- 
+
     public ExamOneReview()
     {
         // initialise instance variables
-        
+
     }
-    
+
     public static void main(){
-    // #1 Ex.1
-    //boolean test = 7 < 8;
-    
-    // #1 Ex.2
-    //boolean test = 7; not appropriate value
-    //boolean test == true; not appropriate value
-    //int x = 7.23;double,not int
-    //int val = 7/12.0;  can't divide int by double
-    //double num1 = num2%3; // works
-    
-    
-    // #2
-    //Pumpkin p = new Pumpkin(3);
-    
-    // #3
-    // public class Numbers{
+        // #1 Ex.1
+        //boolean test = 7 < 8;
+
+        // #1 Ex.2
+        //boolean test = 7; not appropriate value
+        //boolean test == true; not appropriate value
+        //int x = 7.23;double,not int
+        //int val = 7/12.0;  can't divide int by double
+        //double num1 = num2%3; // works
+
+        // #2
+        //Pumpkin p = new Pumpkin(3);
+
+        // #3
+        // public class Numbers{
         // //1.  Create two instance variables of type int
         // private int x, y;
         // //2.  Create a constructor that takes in two int parameters and sets the instance variables equal to the input parameters.
@@ -45,93 +44,115 @@ public class ExamOneReview
         // {
         // // put your code here
         // return x * y;
-    // }
-    
-    // #4 Ex.1
-    // int num =12345;
-    // int i = 0;
-    
-    // while (num > 0){
-            // i++;
-            // num=num/10;
+        // }
+
+        // #4 Ex.1
+        // int num =12345;
+        // int i = 0;
+
+        // while (num > 0){
+        // i++;
+        // num=num/10;
         // }
         // System.out.print("count: " + i);
-        
-    // #4 ex2
-    // int num =12345;
-    // int i = 0;
-    //sum =0;
-    // while (num != 0){
-            
-            // sum+=num%10;
-            // num=num/10;
-            
-        // }
-        // System.out.print("Sum: " + sum);
-        
-       // #5
-    //public static int sum(){
-           // int num =12345;
-           // int i = 0;
+
+        // #4 ex2
+        // int num =12345;
+        // int i = 0;
         //sum =0;
         // while (num != 0){
-            
-            // sum+=num%10;
-            // num=num/10;
-            
+
+        // sum+=num%10;
+        // num=num/10;
+
+        // }
+        // System.out.print("Sum: " + sum);
+
+        // #5
+        //public static int sum(){
+        // int num =12345;
+        // int i = 0;
+        //sum =0;
+        // while (num != 0){
+
+        // sum+=num%10;
+        // num=num/10;
+
         // }
         // return sum;
         //}
-        
+
         // #6
         // public int add(int num1, int num2)
-    // {
+        // {
         // // put your code here
         // return num1 + num2;
-    // }
-    
-    // public int subtract(int num1, int num2)
-    // {
+        // }
+
+        // public int subtract(int num1, int num2)
+        // {
         // // put your code here
         // return num1 - num2;
-    // }
-    
-    // public int multiply(int num1, int num2)
-    // {
+        // }
+
+        // public int multiply(int num1, int num2)
+        // {
         // // put your code here
         // return num1 * num2;
-    // }
-    // public String divide(int num1, int num2)
-    // {
+        // }
+        // public String divide(int num1, int num2)
+        // {
         // // put your code here
         // if (num2 != 0){
         // return "Quotient: " + (num1 / num2);
-    // }else{
-            // return "Cannot divide by zero";
+        // }else{
+        // return "Cannot divide by zero";
         // }
-        
-    // #7
-        // int cntrl= 0, num = 5;
-// while(cntrl < num ){
-  // cntrl++;
-  // System.out.print("num = " + num + ", cntrl = " + cntrl);
-// }
-//output:num = 5, cntrl = 1num = 5, cntrl = 2num = 5, cntrl = 3num = 5, cntrl = 4num = 5, cntrl = 5
-        
-// #8
-// double num1 = 8.0, num2 = 8.0;
-// if(num1 > num2)
-// System.out.println("Peace Out");
-// else if(num1 <= num2)
-// System.out.println("Far Out");
-         // else if(num1 == num2)
-    			// System.out.println("Space Out");
-         // else 
-    			// System.out.println("Freak Out");
-//output: Far Out
-// #9
 
+        // #7
+        // int cntrl= 0, num = 5;
+        // while(cntrl < num ){
+        // cntrl++;
+        // System.out.print("num = " + num + ", cntrl = " + cntrl);
+        // }
+        //output:num = 5, cntrl = 1num = 5, cntrl = 2num = 5, cntrl = 3num = 5, cntrl = 4num = 5, cntrl = 5
+
+        // #8
+        // double num1 = 8.0, num2 = 8.0;
+        // if(num1 > num2)
+        // System.out.println("Peace Out");
+        // else if(num1 <= num2)
+        // System.out.println("Far Out");
+        // else if(num1 == num2)
+        // System.out.println("Space Out");
+        // else 
+        // System.out.println("Freak Out");
+        //output: Far Out
+        // #9
+        // int nums[];
+        // nums = new int[100];
+        // for(int i = 0; i < nums.length; i++){     //  length 10
+        // nums[i] = (int)(Math.random()*10) + 1;  //  last index = 9
+        // }
+        // // #10
+        // for(int i = 0; i < nums.length; i++){ 
+        // System.out.print(nums[i] + ", ");//  print all nums
+        // }
+        // // #11
+        // int sum = 0;
+        // for(int i = 0; i < nums.length; i++){ 
+        // sum = sum + nums[i];  //  add all in nums to values to sum
+        // }
+        // System.out.print("sum: " + sum);
+        // // #12
+        // public int[] copyArray(int[] origArr){
+        // private int[] newArray;
+        // newArray = new newArray[nums.length];
+        // for(int i = 0; i < nums.length; i++){     //  length 10
+        // newArray[i] = nums[i];  //  last index = 9
+        // }
+        // return newArray[1];
+        // }
     }
-     }
-    
+}
 

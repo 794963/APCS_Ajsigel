2d25434b6329346576f1b44a246417d1f8224e52
794963 <51171863+794963@users.noreply.github.com>
2019-09-19 22:33:18 -0700

    rgs

--- lab 918/NestedLoops.java
+++ lab 918/NestedLoops.java
@@ -5,6 +5,7 @@
  * @author (Jaan Sigel)
  * @version (918)
  */
+import java.lang.Math;
 public class NestedLoops
 {
     // instance variables - replace the example below with your own
@@ -20,8 +21,9 @@ public class NestedLoops
     }
     
     public static void main(){
-        int n = 10;
-        probC(n);
+        // int n = 7;
+        // probE(n);
+        probF();
     }
     public static void probA(){
         for(int i=1;i<=5;i++){
@@ -47,4 +49,46 @@ public class NestedLoops
         System.out.println();
         }
     }
+     public static void probD(int n){
+        for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+        System.out.print("-");
+        }
+        System.out.print(i+1);
+        System.out.println();
+        }
+    }
+    public static void probE(int n){
+        for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+        System.out.print(" ");
+        }
+        for(int h=0;h<i;h++){
+        System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
+    public static void probF(){
+        int x = 1;
+        int y = 0;
+        for(int i=0;i<13;i++){
+        for(int j=0;j< Math.abs(6-i);j++){
+        System.out.print(" ");
+        }
+        for(int h=0;h<x;h++){
+        System.out.print("*");
+        }
+        if (x<13 && y == 0){
+        x+=2;
+    } else if (x>=13){
+    x-=2;
+    y = 1;
+    } else{
+    x-=2;
+    }
+        System.out.println();
+        }
+    }
+    
 }
